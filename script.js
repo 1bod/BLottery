70d18c651d6de8ae166f52b2956ba8d295e97c3e
Maxence Raynal <mraynal2008@gmail.com>
2020-01-04 11:58:08 +0100

    Last number pressed interface added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
+    $(".hist").hide();
     $("td").click(function () {
+        $(".hist").show();
         $(this).toggleClass("checked");
+        $(this).addClass("LP");
+        $(".lastPressed").html($(".LP").html());
+        $(this).removeClass("LP");
     });
 });
