764a235d6bbdfef6d9837592546bf6e9fcedfd58
Maxence Raynal <mraynal2008@gmail.com>
2020-01-18 15:29:33 +0100

    Revert "Chrome scrollbar color is now compatible"
    This reverts commit 4c8488c8b6de2361db33909f59f5de233554e760.

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-var elem = document.documentElement; 
-
 $(document).ready(function () {
-    $(".lastPressed").hide();
+    $(".hist").hide();
     $("td").click(function () {
-        $(".lastPressed").show();
+        $(".hist").show();
         $(this).toggleClass("checked");
         $(this).addClass("LP");
         $(".lastPressed").html($(".LP").html());
         $(this).removeClass("LP");
     });
-
 });
-
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { /* Firefox */
-        elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE/Edge */
-        elem.msRequestFullscreen();
-    }
-}
-
-function closeFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) { /* Firefox */
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE/Edge */
-        document.msExitFullscreen();
-    }
-}
